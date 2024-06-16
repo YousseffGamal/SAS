4fecb1dfe80135be7120ff38752bad5aa7c806db
YousseffGamal <youssefggamal552@gmail.com>
2024-06-17 01:15:46 +0300

    sd

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
+import 'animate.css/animate.min.css';
 import './Login.css'; // for custom styling
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Login = () => {
+ 
+
     return (
-        <Container className="login-container">
-            <Form className="login-form">
-                <h2>Sign In</h2>
-                <Form.Group controlId="formBasicUsername">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Enter username" />
+        <Container className="login-container" style={{ marginBottom: "40px" }}>
+            <Form className="login-form wow animate__animated animate__fadeInUp">
+                <h2 className='login-Title wow animate__animated animate__fadeInDown'>Sign In</h2>
+                <p className='login-supTitle wow animate__animated animate__fadeInDown' data-wow-delay="0.2s">
+                    Take control of your work <br />
+                    schedule with Saas!
+                </p>
+                <Form.Group controlId="formBasicUsername" style={{ marginTop: "70px" }}>
+                    <Form.Label className="form-label-left wow animate__animated animate__fadeInLeft" data-wow-delay="0.4s">Username</Form.Label>
+                    <Form.Control className='int wow animate__animated animate__fadeInLeft' type="text" placeholder="Enter username" data-wow-delay="0.4s" />
                 </Form.Group>
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                <Form.Group controlId="formBasicPassword" style={{ marginTop: "60px" }}>
+                    <Form.Label className="form-label-left wow animate__animated animate__fadeInLeft" data-wow-delay="0.6s">Password</Form.Label>
+                    <Form.Control className='int wow animate__animated animate__fadeInLeft' type="password" placeholder="Password" data-wow-delay="0.6s" />
                 </Form.Group>
+                <p className='forget wow animate__animated animate__fadeInRight' data-wow-delay="0.8s">Forget your password!</p>
 
-                <Button variant="primary" type="submit">
-                    Submit
+                <Button variant="primary" className='Sigin wow animate__animated animate__fadeInUp' type="submit" data-wow-delay="1s">
+                    Sign in
                 </Button>
             </Form>
         </Container>
