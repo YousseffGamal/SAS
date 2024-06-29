ee71e5958c4c1d4bad9da139bcf41b5a198f2681
Hamdi-Hossam <hamdihossam461@gmail.com>
2024-06-29 18:36:31 +0300

    "Added API call to fetch employee records and replaced hardcoded data with dynamic data from API response"

--- src/Pages/Employees Records/Employees Records.jsx
+++ src/Pages/Employees Records/Employees Records.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from '../../Components/SIdebar/Sidebar';
 import './EmployeesRecords.css';
 import arrow from '../../Images/shortcut.png'
+import api from "../../axios";
 const EmployeesRecords = () => {
+const [records , setRecords]=useState([])
+  useEffect(() => {
+    const records = async () => {
+      try {
+        const res = await api.get('/attendance/get_attendance')
+        console.log(res.data);
+        setRecords(res.data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    records()
+  },[])
+
   return (
     <div className="home-container">
       <Sidebar />
@@ -19,64 +34,16 @@ const EmployeesRecords = () => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>John Doe</td>
-                <td className='gre'>40</td>
-                <td style={{textAlign:"center"}}>9:00 AM - 5:00 PM</td>
-                <td> <img style={{float:"right"}} src={arrow} alt="" /></td>
-
-              </tr>
-              <tr>
-                <td>Jane Smith</td>
-                <td className='gre'>35</td>
-                <td style={{textAlign:"center"}}>10:00 AM - 6:00 PM</td>
-                <td> <img style={{float:"right"}} src={arrow} alt="" /></td>
-
-              </tr>
-              <tr>
-                <td>Michael Johnson</td>
-                <td className='gre'>38</td>
-                <td style={{textAlign:"center"}}>8:30 AM - 4:30 PM</td>
-                <td> <img style={{float:"right"}} src={arrow} alt="" /></td>
-
-              </tr>
-              <tr>
-                <td>Sarah Brown</td>
-                <td className='gre'>37</td>
-                <td style={{textAlign:"center"}}>9:30 AM - 5:30 PM</td>
-                <td> <img style={{float:"right"}} src={arrow} alt="" /></td>
-
-              </tr>
-              <tr>
-                <td>Chris Lee</td>
-                <td className='gre'>42</td>
-                <td style={{textAlign:"center"}}>9:00 AM - 5:00 PM</td>
-                <td> <img style={{float:"right"}} src={arrow} alt="" /></td>
-
-              </tr>
-              <tr>
-                <td>Amy Wang</td>
-                <td className='gre'>36</td>
-                <td style={{textAlign:"center"}}>10:30 AM - 6:30 PM</td>
-                <td> <img style={{float:"right"}} src={arrow} alt="" /></td>
-
-              </tr>
-              <tr>
-                <td>David Miller</td>
-                <td className='gre'>39</td>
-                <td style={{textAlign:"center"}}>7:00 AM - 3:00 PM</td>
-                <td> <img style={{float:"right"}} src={arrow} alt="" /></td>
-
-              </tr>
-              <tr>
-                <td>Emily Davis</td>
-                <td className='gre'>34</td>
-                <td style={{textAlign:"center"}}>11:00 AM - 7:00 PM</td>
-                <td> <img style={{float:"right"}} src={arrow} alt="" /></td>
-
-              </tr>
-       
-              {/* Add more rows as needed */}
+              {records && records?.map((record) => {
+                return (
+                  <tr>
+                    <td>{record?.employee_name}</td>
+                    <td className='gre'>{(record?.duration / 3600).toFixed(2)}</td>
+                    <td style={{textAlign:"center"}}>{new Date(record?.check_in_time).toLocaleTimeString()} - {new Date(record?.check_out_time).toLocaleTimeString()}</td>
+                    <td> <img style={{float:"right"}} src={arrow} alt="" /></td>
+                  </tr>
+                )
+              })}
             </tbody>
           </table>
         
